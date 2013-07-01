ab1e6c10728b147fb4e340896f75f6a1a67c8ab5
James Hill <jaed.llih@gmail.com>
2013-06-30 23:14:56 -0300

    Added Splash page and main page.

--- QuizBoardProject/QuizBoard/src/main/java/ca/dal/cs/csci4126/quizboard/MainActivity.java
+++ QuizBoardProject/QuizBoard/src/main/java/ca/dal/cs/csci4126/quizboard/MainActivity.java
@@ -1,15 +1,29 @@
 package ca.dal.cs.csci4126.quizboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settings = (Button) findViewById(R.id.bSettings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openSettingsPoint =
+                       new Intent("ca.dal.cs.csci4126.quizboard.SETTINGS");
+                startActivity(openSettingsPoint);
+            }
+        });
     }
 
 
