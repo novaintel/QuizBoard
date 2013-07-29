c5a0d5349d0ee4976768a994101a7ad5969964e9
James Hill <jaed.llih@gmail.com>
2013-07-29 12:49:52 -0300

    Wrong QuizBoard

--- QuizBoard/src/main/java/br/ufscar/dc/thingbroker/interfaces/ThingBrokerRequestListener.java
+++
@@ -1,6 +0,0 @@
-package br.ufscar.dc.thingbroker.interfaces;
-
-public interface ThingBrokerRequestListener 
-{
-    public void executeAfterRequest(final int serviceId, Object result, Exception ex);
-}
